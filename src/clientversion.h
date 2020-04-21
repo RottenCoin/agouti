41c64feb00b20c2e8f73474f81904b4f14c79b54
RottenCoin <38838284+RottenCoin@users.noreply.github.com>
2020-04-21 15:09:49 +0100

    copyright update

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2017 The Agouti developers
+// Copyright (c) 2018-2020 The Agouti Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
